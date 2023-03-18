e5ab9fb078515a0236e3110ff8f33051e3f8ceea
forward0606 <chen910606@gmail.com>
2023-03-18 23:46:01 +0800

    add install information

---
+++ install/Java-example1/testLexer.java
@@ -0,0 +1,28 @@
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+
+
+public class testLexer {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  try {
+	           //CharStream input = new ANTLRFileStream(args[0]);
+               CharStream input = CharStreams.fromFileName(args[0]);
+               test1 lexer = new test1(input);
+	           Token token = lexer.nextToken();
+	           while (token.getType() != -1) {  // -1 is EOF.
+	             System.out.println("Token: "+token.getType() + " " + token.getText());
+	             token = lexer.nextToken();
+	           }
+	        } catch(Throwable t) {
+	           System.out.println("Exception: "+t);
+	           t.printStackTrace();
+	        }
+	}
+
+}
---
+++ install/readme.md
@@ -0,0 +1,7 @@
+reference: https://tomassetti.me/antlr-mega-tutorial/
+
+install
+```
+curl -O https://www.antlr.org/download/antlr-4.12.0-complete.jar
+```
+編譯與執行請見 Java-example1 中的 makefile
